f03e932a5414b374e9a9a4b0321c27ca7ca1a207
npetrova <nadezhda.petrova@hotmail.com>
2014-12-12 14:03:36 +0200

    Endless pager for issues is replaced with the build in paging

---
+++ DevMagazineSite/DevMagazine.Issues/Mvc/Helpers/IssueHelper.cs
@@ -0,0 +1,36 @@
+﻿using DevMagazine.Issues.Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.DynamicModules;
+using Telerik.Sitefinity.DynamicModules.Model;
+using Telerik.Sitefinity.GenericContent.Model;
+
+namespace DevMagazine.Issues.Mvc.Helpers
+{
+    /// <summary>
+    /// Contains Helper methods for Issues
+    /// </summary>
+    public class IssueHelper
+    {
+        #region Static methods
+        /// <summary>
+        /// Gets the latest issue.
+        /// </summary>
+        /// <returns></returns>
+        public static DynamicContent GetLatestIssue()
+        {
+            var dynamicManager = DynamicModuleManager.GetManager();
+
+            var latestIssue = dynamicManager.GetDataItems(IssueViewModel.IssueType)
+              .Where(di => di.Status == ContentLifecycleStatus.Live)
+              .Where(di => di.Visible == true)
+              .OrderByDescending(di => di.PublicationDate)
+              .FirstOrDefault();
+
+            return latestIssue;
+        }
+        #endregion
+    }
+}
