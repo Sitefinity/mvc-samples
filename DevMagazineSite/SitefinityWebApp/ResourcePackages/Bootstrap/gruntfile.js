80ab252ebb101f98dd5ae97cabdf58259d4961ac
ElenaGaneva <Elena.Ganeva@telerik.com>
2015-10-20 17:40:57 +0300

    Updated dev magazine to 8.2

--- DevMagazineSite/SitefinityWebApp/ResourcePackages/Bootstrap/gruntfile.js
+++ DevMagazineSite/SitefinityWebApp/ResourcePackages/Bootstrap/gruntfile.js
@@ -7,221 +7,198 @@ var path = require('path');
 // e.g. 'bar/foo/**/*.js'
 
 module.exports = function (grunt) {
-    'use strict';
+	'use strict';
 
-    // load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+	// load all grunt tasks
+	require('load-grunt-tasks')(grunt);
 
-    // show elapsed time at the end
-    require('time-grunt')(grunt);
-    // Init
-    grunt.initConfig({
-        timestamp: '<%= new Date().getTime() %>',
-        pkg: grunt.file.readJSON('package.json'),
+	// show elapsed time at the end
+	require('time-grunt')(grunt);
+	// Init
+	grunt.initConfig({
+		timestamp: '<%= new Date().getTime() %>',
+		pkg: grunt.file.readJSON('package.json'),
 
-        src: {
-            path: 'assets/src',
-            sass: '**/*.{scss,sass}',
-        },
-        dist: {
-            path: 'assets/dist'
-        },
+		src: {
+			path       : 'assets/src',
+			sass       : '**/*.{scss,sass}'
+		},
 
-        // clean all generated files
-        clean: {
-            all: {
-                files: [{
-                    src: [
-                      '<%= dist.path %>/**/*.css',
-                      '<%= dist.path %>/**/*.js',
-                      '<%= dist.path %>/**/*.{png,jpg,gif}'
-                    ]
-                }]
-            },
-            sprites: {
-                files: [{
-                    src: [
-                      '<%= dist.path %>/**/*.{png,jpg,gif,jpeg}'
-                    ]
-                }]
-            }
-        },
+		dist: {
+			path       : 'assets/dist'
+		},
 
-        // use always with target e.g. `csslint:doc` or `csslint:dev`
-        // unfortunately there is no point to run csslint on compressed css so
-        // csslint runs once, when you use `grunt` and it lints on documentation's css
-        // csslint runs on every save when you use `grunt dev` and it lints the original file you are working on -> `style.css`
-        csslint: {
-            options: {
-                csslintrc: 'csslint.json'
-            },
-            dev: {
-                src: '<%= dist.path %>/styles.css'
-            }
-        },
+		// clean all generated files
+		clean: {
+			all: {
+				files: [{
+					src: [
+						'<%= dist.path %>/**/*.css',
+						'<%= dist.path %>/**/*.js',
+						'<%= dist.path %>/**/*.{png,jpg,gif,jpeg}'
+					]
+				}]
+			},
+			images: {
+				files: [{
+					src: [
+						'<%= dist.path %>/**/*.{png,jpg,gif,jpeg}'
+					]
+				}]
+			}
+		},
 
-        sass: {
-            dist: {
-                files: {
-                    '<%= dist.path %>/css/styles.css': 'assets/src/sass/styles.scss'
-                }
-            }
-        },
+		sass: {
+			options: {
+				outputStyle: 'nested'
+			},
+			dist: {
+				files: {
+					'<%= dist.path %>/css/styles.css': '<%= src.path %>/sass/styles.sass'
+				}
+			}
+		},
 
-        uncss: {
-            dist: {
-                options: {
-                    ignore: ['#added_at_runtime', /test\-[0-9]+/],
-                    media: ['(min-width: 700px) handheld and (orientation: landscape)'],
-                    csspath: '../../../',
-                    stylesheets: ['<%= dist.path %>/css/styles.css'],
-                    ignoreSheets: [/fonts.googleapis/],
-                    // urls         : ['http://localhost:3000/mypage', '...'], // Deprecated
-                    timeout: 1,
-                    report: 'gzip'
-                },
-                files: {
-                    '<%= dist.path %>/css/styles.tidy.css': 'MVC/**/*.cshtml'
-                }
-            }
-        },
+		// use always with target e.g. `csslint:doc` or `csslint:dev`
+		// unfortunately there is no point to run csslint on compressed css so
+		// csslint runs once, when you use `grunt` and it lints on documentation's css
+		// csslint runs on every save when you use `grunt dev` and it lints the original file you are working on -> `style.css`
+		csslint: {
+			options: {
+				csslintrc: 'csslint.json'
+			},
+			dev: {
+				src: ['<%= src.sitefinity %>']
+			}
+		},
 
-        cssmin: {
-            minify: {
-                expand: true,
-                cwd: '<%= dist.path %>/css/',
-                src: ['*.css', '!*.min.css'],
-                dest: '<%= dist.path %>/css/',
-                ext: '.min.css'
-            }
-        },
+		webfont: {
+			icons: {
+				src: 'assets/src/icons/*.svg',
+				dest: 'assets/dist/fonts/',
+				destCss: 'assets/src/sass/icons/',
+				options: {
+					destHtml: '',
+					engine: 'node',
+					font: 'sf-icon-font',
+					stylesheet: 'scss',
+					partialPrefix: true,
+					relativeFontPath: '../fonts/',
+					template: 'feather-icons.css',
+					types: 'eot,woff,ttf,svg',
+					order: 'eot,woff,ttf,svg',
+					startCodepoint: 0x00b1,
+					normalize: true,
+					fontHeight: 4096,
+					ascent: 4096,
+					descent: 0
+				}
+			}
+		},
 
-        // Concat & minify
-        // this processes only the files described in 'jsfiles.json'
-        uglify: {
-            options: {
-                report: 'gzip',
-                warnings: true
-            },
-            dist: {
-                options: {
-                    mangle: true,
-                    compress: true
-                },
-                files: {
-                    '<%= dist.path %>/js/all.min.js': ['<%= src.path %>/js/{,*/}*.js']
-                }
-            }
-        },
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: '<%= dist.path %>/css/',
+				src: ['*.css', '!*.min.css'],
+				dest: '<%= dist.path %>/css/',
+				ext: '.min.css'
+			}
+		},
 
-        // Image Optimization
-        imagemin: {
-            dist: {
-                options: {
-                    optimizationLevel: 4,
-                    progressive: true
-                },
-                files: [
-                  {
-                      expand: true,
-                      cwd: '<%= src.path %>/images',
-                      src: ['**/*.{png,jpg,gif,jpeg}'],
-                      dest: '<%= dist.path %>/images'
-                  }
-                ]
-            }
-        },
+		// Concat & minify
+		// this processes only the files described in 'jsfiles.json'
+		uglify: {
+			options: {
+				report: 'gzip',
+				warnings: true
+			},
+			dist: {
+				options: {
+					mangle: true,
+					compress: true
+				},
+				files: {
+					'<%= dist.path %>/js/bootstrap.min.js':'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
+				}
+			}
+		},
 
-        webfont: {
-            icons: {
-                src: '<%= src.path %>/icons/*.svg',
-                dest: 'assets/dist/fonts',
-                destCss: '<%= src.path %>/sass/modules/',
-                options: {
-                    destHtml: './',
-                    engine: 'node',
-                    font: 'icon-font',
-                    stylesheet: 'scss',
-                    partialPrefix: true,
-                    relativeFontPath: '../fonts/',
-                    template: 'icon-font.css',
-                    order: 'eot,svg,woff,ttf',
-                    startCodepoint: 0x00b1,
-                    normalize: true,
-                    // if you decide to change the next two 4096 numbers I will murder you
-                    fontHeight: 4096,
-                    ascent: 4096,
-                    descent: 0
-                }
-            }
-        },
+		// Image Optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 4,
+					progressive: true
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'assets/src/images',
+						src: ['**/*.{png,jpg,gif,jpeg}'],
+						dest: 'assets/dist/images'
+					}
+				]
+			}
+		},
 
-        // Sprite generation
-        sprite: {
-            all: {
-                src: 'assets/src/images/social-share/*.png',
-                dest: 'assets/src/images/social-share-sprite.png',
-                destCss: 'assets/src/sass/_sf-social-share-sprite.sass',
-                cssTemplate: 'assets/src/sass/social-share-sprite.mustache'
-            }
-        },
+		// Sprite generation
+		sprite:{
+			all: {
+				src: 'assets/src/images/sprite/*.png',
+				dest: 'assets/src/images/sprite.png',
+				destCss: 'assets/src/sass/_sf-sprite.sass',
+				cssTemplate: 'assets/src/sass/sf-sprite.mustache'
+			}
+		},
 
-        watch: {
-            options: {
-                spawn: false
-            },
-            styles: {
-                files: ['<%= src.path %>/**/*.{scss,sass}'],
-                tasks: ['sass:dist', 'cssmin']
-                // tasks: ['sass:dist', 'uncss', 'cssmin']
-            },
+		watch: {
+			options: {
+				spawn: false
+			},
+			styles: {
+				files: ['<%= src.path %>/**/*.{scss,sass}'],
+				tasks: ['sass:dist', 'cssmin']
+			},
+			images: {
+				files: ['<%= src.path %>/**/*.{png,jpg,gif,jpeg}'],
+				tasks: ['clean:images', 'sprite', 'imagemin']
+			},
+			js: {
+				files: ['<%= src.path %>/**/*.js'],
+				tasks: ['uglify:dist']
+			},
+		},
 
-            images: {
-                files: ['<%= src.path %>/**/*.{png,jpg,gif,jpeg}'],
-                tasks: ['clean:images', 'sprite', 'imagemin']
-            },
+		concurrent: {
+			dev: {
+				tasks: ['watch:styles', 'watch:js', 'watch:images'],
+				options: {
+					logConcurrentOutput: true
+				}
+			}
+		}
+	});
 
-            js: {
-                files: ['<%= src.path %>/**/*.js'],
-                tasks: ['uglify:dist']
-            },
-        },
+	// Tasks
+	grunt.registerTask('iconfont', [
+		'webfont',
+		'build'
+	]);
 
-        concurrent: {
-            dev: {
-                tasks: ['watch:styles', 'watch:js', 'watch:images'],
-                options: {
-                    logConcurrentOutput: true
-                }
-            }
-        }
-    });
+	// Runs once
+	grunt.registerTask('build', [
+		'newer:sprite',
+		'sass:dist',
+		'cssmin',
+		'uglify:dist',
+		'newer:imagemin'
+	]);
 
-    grunt.registerTask('iconfont', [
-      'webfont'
-    ]);
-
-
-
-    // Tasks
-    grunt.registerTask('default', [
-      'clean:all',
-      'newer:sprite',
-      'sass:dist',
-      // 'uncss',
-      'cssmin',
-      'uglify:dist',
-      'newer:csslint:dev',
-      'newer:imagemin',
-      'concurrent:dev'
-    ]);
-
-    // Run this task to strip the CSS of unused classes and to minify it
-    grunt.registerTask('stripcss', [
-      'clean:all',
-      'sass:dist',
-      'uncss',
-      'cssmin'
-    ]);
-
+	// default task runs csslint once on startup on documentation's css
+	grunt.registerTask('default', [
+		// 'clean:all',
+		'build',
+		'concurrent:dev'
+	]);
 };
