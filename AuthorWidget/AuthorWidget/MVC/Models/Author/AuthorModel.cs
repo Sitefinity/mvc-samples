a559bac2ca10182b1018bbd441145708f15dd6d0
dzhenko <dzhenko@gmail.com>
2015-12-01 14:21:59 +0200

    Author widget added

---
+++ AuthorWidget/AuthorWidget/MVC/Models/Author/AuthorModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Web;
+using Telerik.Sitefinity.Modules.Blogs;
+using Telerik.Sitefinity.Modules.GenericContent;
+using Telerik.Sitefinity.Modules.Libraries;
+using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.Web;
+
+namespace AuthorWidget.MVC.Models.Author
+{
+    public class AuthorModel
+    {
+        public Guid PageId { get; set; }
+
+        public string AmazonUrl { get; set; }
+
+        public string ImageProviderName { get; set; }
+
+        public Guid ImageId { get; set; }
+
+        [DynamicLinksContainer]
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public string CssClass { get; set; }
+
+        public AuthorViewModel GetViewModel()
+        {
+            return new AuthorViewModel()
+            {
+                PageUrl = this.PageUrl(),
+                Description = this.Description,
+                ImageUrl = this.GetImageUrl(),
+                AmazonUrl = this.AmazonUrl,
+                Name = this.Name,
+                CssClass = this.CssClass
+            };
+        }
+
+        private string PageUrl()
+        {
+            if (this.PageId != Guid.Empty)
+	        {
+                var siteMap = SitefinitySiteMap.GetCurrentProvider();
+
+                SiteMapNode node;
+                var sitefinitySiteMap = siteMap as SiteMapBase;
+                if (sitefinitySiteMap != null)
+                    node = sitefinitySiteMap.FindSiteMapNodeFromKey(this.PageId.ToString(), false);
+                else
+                    node = siteMap.FindSiteMapNodeFromKey(this.PageId.ToString());
+
+                if (node != null)
+                    return UrlPath.ResolveUrl(node.Url, true);
+	        }
+
+            return string.Empty;
+        }
+
+        private string GetImageUrl()
+        {
+            if (this.ImageId != Guid.Empty)
+            {
+                var image = LibrariesManager.GetManager(this.ImageProviderName).GetImage(this.ImageId);
+                if (image != null)
+                {
+                    return image.Url;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ AuthorWidget/AuthorWidget/MVC/Models/Author/AuthorViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AuthorWidget.MVC.Models.Author
+{
+    public class AuthorViewModel
+    {
+        public string PageUrl { get; set; }
+
+        public string AmazonUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public string CssClass { get; set; }
+    }
+}
