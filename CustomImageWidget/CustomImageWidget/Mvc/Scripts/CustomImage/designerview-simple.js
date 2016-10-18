de9aefb24b675177890d4c4893a2dd5668b12c88
manev <manev@progress.com>
2016-10-18 11:35:25 +0300

    Add missing "ngSanitize" to the designer module collection.

--- CustomImageWidget/CustomImageWidget/Mvc/Scripts/CustomImage/designerview-simple.js
+++ CustomImageWidget/CustomImageWidget/Mvc/Scripts/CustomImage/designerview-simple.js
@@ -1,3 +1,6 @@
 ﻿var designerModule = angular.module('designer');
 angular.module('designer').requires.push('sfFields');
 angular.module('designer').requires.push('sfSelectors');
+
+//// NOTE: Use this code only with Sitefinity version 9.1 or above. Otherwise the "ngSanitize" module should no be included. 
+angular.module('designer').requires.push('ngSanitize');
