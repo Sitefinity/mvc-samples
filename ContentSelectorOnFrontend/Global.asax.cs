6d99afe29a6e08e001deffa4434ae8f8952db884
ElenaGaneva <Elena.Ganeva@telerik.com>
2016-10-18 18:40:45 +0300

    Add new sample for using selector on frontend

---
+++ ContentSelectorOnFrontend/Global.asax.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Telerik.Sitefinity.Abstractions;
+
+namespace SitefinityWebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Initialized += Bootstrapper_Initialized;
+        }
+
+        void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
+        {
+            if (e.CommandName == "Bootstrapped")
+            {
+                if (System.Web.Routing.RouteTable.Routes["Classic"] == null)
+                    System.Web.Mvc.RouteCollectionExtensions.MapRoute(System.Web.Routing.RouteTable.Routes,
+                 "Classic",
+                 "app",
+                 new { controller = "App", action = "Index" }
+             );
+            }
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ContentSelectorOnFrontend/Mvc/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
