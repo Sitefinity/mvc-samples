5c872024b309645a4dd7a959d8bb2594da27d587
Vasil Dinev <vassildinev@gmail.com>
2017-05-16 14:53:04 +0300

    Added upgraded samples

--- DevMagazineSite/SitefinityWebApp/Properties/AssemblyInfo.cs
+++ DevMagazineSite/SitefinityWebApp/Properties/AssemblyInfo.cs
@@ -37,6 +37,10 @@ using System.Resources;
 
 
 
+
+
+
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
