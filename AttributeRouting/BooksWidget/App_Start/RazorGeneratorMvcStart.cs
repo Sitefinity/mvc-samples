74b296d0c2acb75bf235b572a78e90abf7dfe036
vassildinev <vassildinev@gmail.com>
2017-11-16 11:05:59 +0200

    Upgrade BooksWidget to Sitefinity 10.2

--- AttributeRouting/BooksWidget/App_Start/RazorGeneratorMvcStart.cs
+++ AttributeRouting/BooksWidget/App_Start/RazorGeneratorMvcStart.cs
@@ -5,10 +5,14 @@ using RazorGenerator.Mvc;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(typeof(BooksWidget.RazorGeneratorMvcStart), "Start")]
 
-namespace BooksWidget {
-    public static class RazorGeneratorMvcStart {
-        public static void Start() {
-            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+namespace BooksWidget
+{
+    public static class RazorGeneratorMvcStart
+    {
+        public static void Start()
+        {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly)
+            {
                 UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
             };
 
