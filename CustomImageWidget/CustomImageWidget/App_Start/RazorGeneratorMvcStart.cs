bda1e4d6ad712d640520d10fe245e9097411aa12
vassildinev <vassildinev@gmail.com>
2017-11-22 16:32:00 +0200

    Upgrade CustomImageWidget

--- CustomImageWidget/CustomImageWidget/App_Start/RazorGeneratorMvcStart.cs
+++ CustomImageWidget/CustomImageWidget/App_Start/RazorGeneratorMvcStart.cs
@@ -5,10 +5,14 @@ using RazorGenerator.Mvc;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(typeof(CustomImageWidget.RazorGeneratorMvcStart), "Start")]
 
-namespace CustomImageWidget {
-    public static class RazorGeneratorMvcStart {
-        public static void Start() {
-            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+namespace CustomImageWidget
+{
+    public static class RazorGeneratorMvcStart
+    {
+        public static void Start()
+        {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly)
+            {
                 UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
             };
 
