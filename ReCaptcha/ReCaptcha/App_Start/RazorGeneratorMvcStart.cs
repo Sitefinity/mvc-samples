209d7d0f520f06e241159497ba558bdea519fa5f
vassildinev <vassildinev@gmail.com>
2017-11-23 08:55:49 +0200

    Upgrade ReCaptcha sample

--- ReCaptcha/ReCaptcha/App_Start/RazorGeneratorMvcStart.cs
+++ ReCaptcha/ReCaptcha/App_Start/RazorGeneratorMvcStart.cs
@@ -5,10 +5,14 @@ using RazorGenerator.Mvc;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(typeof(ReCaptcha.RazorGeneratorMvcStart), "Start")]
 
-namespace ReCaptcha {
-    public static class RazorGeneratorMvcStart {
-        public static void Start() {
-            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
+namespace ReCaptcha
+{
+    public static class RazorGeneratorMvcStart
+    {
+        public static void Start()
+        {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly)
+            {
                 UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
             };
 
