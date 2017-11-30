4bb955d9f4afd19fdfb7d8153d10086ba549b0db
vassildinev <vassildinev@gmail.com>
2017-11-30 12:38:35 +0200

    Fix KendoBooks widget on frontend

--- KendoBooksWidget/KendoBooksWidget/Mvc/Scripts/kendo-books-widget.js
+++ KendoBooksWidget/KendoBooksWidget/Mvc/Scripts/kendo-books-widget.js
@@ -1,9 +1,11 @@
-﻿function vote(e) {
-    e.preventDefault();
+﻿(function () {
+    window.vote = function (e) {
+        e.preventDefault();
 
-    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-    $.post(sf_appPath + 'web-interface/books/vote/' + dataItem.Title, function (data) {
-        $('#booksGrid').data('kendoGrid').dataSource.read();
-        $("#booksGrid").data('kendoGrid').refresh();
-    });
-}
+        var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+        $.post(sf_appPath + 'web-interface/books/vote/' + dataItem.Title, function (data) {
+            $('#booksGrid').data('kendoGrid').dataSource.read();
+            $("#booksGrid").data('kendoGrid').refresh();
+        });
+    };
+}());
