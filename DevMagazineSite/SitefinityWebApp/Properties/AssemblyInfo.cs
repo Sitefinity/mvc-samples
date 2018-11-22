6fbd8402e966689d4561beb0c9bcae984eb38b88
vassildinev <vassildinev@gmail.com>
2018-11-22 09:57:52 +0200

    Upgrade DevMagazine

--- DevMagazineSite/SitefinityWebApp/Properties/AssemblyInfo.cs
+++ DevMagazineSite/SitefinityWebApp/Properties/AssemblyInfo.cs
@@ -36,6 +36,8 @@ using System.Resources;
 
 
 
+
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
