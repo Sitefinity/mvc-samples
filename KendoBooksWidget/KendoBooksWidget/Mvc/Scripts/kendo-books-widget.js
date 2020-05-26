294a5ebdaff821dc81db9236bcf79c53799fb37f
Julia Meleshko <julia.meleshko@progress.com>
2020-05-26 18:52:05 +0300

    bugfix in kendo books widget sample

--- KendoBooksWidget/KendoBooksWidget/Mvc/Scripts/kendo-books-widget.js
+++ KendoBooksWidget/KendoBooksWidget/Mvc/Scripts/kendo-books-widget.js
@@ -2,6 +2,8 @@
     window.vote = function (e) {
         e.preventDefault();
 
+        var sf_appPath = $('#sfAppPath').val();
+
         var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
         $.post(sf_appPath + 'web-interface/books/vote/' + dataItem.Title, function (data) {
             $('#booksGrid').data('kendoGrid').dataSource.read();
